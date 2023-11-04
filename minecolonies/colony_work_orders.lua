3ba3fa62b7927b96a41d8204bbfe4313ea3a00f6
Nova <mariofluddd@gmail.com>
2023-11-04 12:27:55 -0400

    init
    init colonies

---
+++ minecolonies/colony_work_orders.lua
@@ -0,0 +1,15 @@
+local colint = peripheral.find("colonyIntegrator")
+
+if(colint == nil) then
+    printError("Colony Integrator not found.")
+    sleep(10)
+    shutdown()
+end
+
+local Menu = "main"
+
+while(Menu == "main") do
+
+elseif(Menu == "ababab") do
+
+end
