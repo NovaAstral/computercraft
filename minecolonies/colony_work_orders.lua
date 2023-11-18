9c9eefde2aa1d1fd0b96925eed75626c294339a2
NovaAstral <mariofluddd@gmail.com>
2023-11-18 14:24:33 -0400

    finish colony work orders

--- minecolonies/colony_work_orders.lua
+++ minecolonies/colony_work_orders.lua
@@ -1,41 +1,25 @@
 local colint = peripheral.find("colonyIntegrator")
 local mon = peripheral.find("monitor")
-local Menu = "main"
 
 function GetBuildings()
-    if(Menu == "main") then
-        local buildings = colint.getBuildings()
+    local buildings = colint.getBuildings()
 
-        for k,v in ipairs(buildings) do
-            if(v.name == "com.minecolonies.building.builder") then
-                res = colint.getBuilderResources(v.location)
+    for k,v in ipairs(buildings) do
+        if(v.name == "com.minecolonies.building.builder") then
+            res = colint.getBuilderResources(v.location)
 
-                for k,v in pairs(res) do
-                    if(v.status == "NEED_MORE" or v.status == "DONT_HAVE" and v.delivering == 0) then
-                        if(v.status == "NEED_MORE") then
-                            statusstr = "Need More"
-                        else
-                            statusstr = "Don't Have"
-                        end
-
-                        mon.write("["..v.displayName.."]".." Need: ".."["..v.needed.."] "..statusstr) -- item name
-                        curx, cury = mon.getCursorPos()
-                        mon.setCursorPos(1,cury+1)
-
-                    end
+            for k,v in pairs(res) do
+                if(v.status == "NEED_MORE" or v.status == "DONT_HAVE" and v.delivering == 0) then
+                    mon.write("["..v.displayName.."]".." Need: ".."["..v.needed.."] ") -- item name
+                    curx, cury = mon.getCursorPos()
+                    mon.setCursorPos(1,cury+1)
                 end
             end
-            
-            --curx, cury = mon.getCursorPos()
-            --print(v.name)
-            --mon.write(v.name)
-            --mon.setCursorPos(1,cury+1)
         end
     end
 
-    if(Menu == "ababab") then
-        print("not main")
-    end
+    os.sleep(5)
+    os.reboot()
 end
 
 if(mon == nil) then
