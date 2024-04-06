6a995ff6188bad6885b7c817d8db0b75b9e0806e
NovaAstral <mariofluddd@gmail.com>
2024-04-06 15:50:10 -0300

    dialer update

--- stargate_dialer.lua
+++ stargate_dialer.lua
@@ -39,41 +39,41 @@ local function split(str,sep)
 end
 
 local function dial(tbl) --point of origin is 0
-    if(#tbl == 7) then -- index 1 is a space
+    if(#tbl == 7) then -- index 1 is 'dial'
         print("Dialing Intergalactic Address "..tbl[2].."-"..tbl[3].."-"..tbl[4].."-"..tbl[5].."-"..tbl[6].."-"..tbl[7].."-0")
 
-        os.sleep(1)
+        os.sleep(0.5)
         Stargate.engageSymbol(tonumber(tbl[2]))
-        os.sleep(1)
+        os.sleep(0.5)
         Stargate.engageSymbol(tonumber(tbl[3]))
-        os.sleep(1)
+        os.sleep(0.5)
         Stargate.engageSymbol(tonumber(tbl[4]))
-        os.sleep(1)
+        os.sleep(0.5)
         Stargate.engageSymbol(tonumber(tbl[5]))
-        os.sleep(1)
+        os.sleep(0.5)
         Stargate.engageSymbol(tonumber(tbl[6]))
-        os.sleep(1)
+        os.sleep(0.5)
         Stargate.engageSymbol(tonumber(tbl[7]))
-        os.sleep(1)
+        os.sleep(0.5)
         Stargate.engageSymbol(0)
     elseif(#tbl == 8) then
         print("Dialing Extragalactic Address "..tbl[2].."-"..tbl[3].."-"..tbl[4].."-"..tbl[5].."-"..tbl[6].."-"..tbl[7].."-"..tbl[8].."-0")
 
-        os.sleep(1)
+        os.sleep(0.5)
         Stargate.engageSymbol(tonumber(tbl[2]))
-        os.sleep(1)
+        os.sleep(0.5)
         Stargate.engageSymbol(tonumber(tbl[3]))
-        os.sleep(1)
+        os.sleep(0.5)
         Stargate.engageSymbol(tonumber(tbl[4]))
-        os.sleep(1)
+        os.sleep(0.5)
         Stargate.engageSymbol(tonumber(tbl[5]))
-        os.sleep(1)
+        os.sleep(0.5)
         Stargate.engageSymbol(tonumber(tbl[6]))
-        os.sleep(1)
+        os.sleep(0.5)
         Stargate.engageSymbol(tonumber(tbl[7]))
-        os.sleep(1)
+        os.sleep(0.5)
         Stargate.engageSymbol(tonumber(tbl[8]))
-        os.sleep(1)
+        os.sleep(0.5)
         Stargate.engageSymbol(0)
     else
         print("Invalid Address!")
@@ -83,7 +83,7 @@ local function dial(tbl) --point of origin is 0
 end
 
 while true do
-    if(Menu == 1) then
+    if(Menu == 1) then --string.sub("dial 123",6)
         print("Commands:")
         print("Disconnect / Close - Closes or Resets the Stargate")
         print("Time - Prints how long the Stargate has been open for")
@@ -100,14 +100,16 @@ while true do
             cmds = split(cmd," ")
         end
 
-        if(string.lower(cmds[1]) == "disconnect" or string.lower(cmds[1]) == "close") then
+        if(string.lower(cmdtest[1]) == "disconnect" or string.lower(cmds[1]) == "close") then
             print("Closing Stargate")
             Stargate.disconnectStargate()
-        elseif(string.lower(cmds[1]) == "time") then
+        elseif(string.lower(cmdtest[1]) == "time") then
             print("The Stargate has been open for "..Stargate.getOpenTime() / 20.." seconds.")
-        elseif(string.lower(cmds[1]) == "dial") then
+        elseif(string.lower(cmdtest[1]) == "dial") then
             Menu = 2
             dial(cmds)
+        else
+            print("Invalid Command")
         end
     end
 end
